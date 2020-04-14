44a1c83ce84ce864af5f8aa9414f58c1f29859ca
jabupe <jaroslav.buric@gmail.com>
2020-04-14 15:49:00 +0200

    Initial commit

---
+++ library/src/main/java/cz/jb/library/Inform.java
@@ -0,0 +1,10 @@
+package cz.jb.library;
+
+public class Inform {
+    public static String getName() {
+        return "John Breword";
+    }
+    public static int sumTwoNumber(int first, int second) {
+        return first+second;
+    }
+}
